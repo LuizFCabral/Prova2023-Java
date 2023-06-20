a9777d0d38ee5c226b99e4fb8a7ef60078900c2f
LuizFCabral <luizfelipe.cabralsouza@gmail.com>
2023-06-20 08:19:53 -0300

    Initial commit

---
+++ src/test/java/com/fatec/projetojava/ProjetojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.projetojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
